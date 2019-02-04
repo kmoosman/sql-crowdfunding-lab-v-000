d381f59dd340aaa266dff0812c2f4b3404d8d73d
Katie Moosman <moosman.katie@gmail.com>
2019-02-04 01:44:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -50,5 +50,10 @@ WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT category, SUM(pledges.amount)
+FROM projects
+JOIN pledges ON
+  projects.id = pledges.project_id
+GROUP BY(projects.category)
+HAVING projects.category = 'books';"
 end
